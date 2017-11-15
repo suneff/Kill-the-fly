2202680afd1c9e5deff1c525ef8743f8703714b8
suneff <nefelib25@gmail.com>
2017-11-15 15:21:02 +0200

    Add project files.

---
+++ proairetiki4/Form1.Designer.cs
@@ -0,0 +1,103 @@
+﻿namespace proairetiki4
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(217, 198);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(205, 34);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Easy";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(217, 238);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(205, 34);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Normal";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(217, 278);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(205, 34);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "Hard";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 27.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(193, 103);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(245, 42);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "KiLL ThE FLy";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(659, 421);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.Name = "Form1";
+            this.Text = "Kill The Fly!";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ proairetiki4/Form1.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace proairetiki4
+{
+    public partial class Form1 : Form
+    {
+        int dif = 0; //initialize difficulty
+        bool flag = true; //if game is open
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+            dif = 1;
+            createForm();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            dif = 2;
+            createForm();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            dif = 3;
+            createForm();
+        }
+
+        private void createForm()
+        {
+            Form2 game = new Form2(dif);
+            game.Show();
+            this.Hide();
+           
+        //disable th forma 1 etsi wste na mi mporei na anoiksei 
+        //polles fores to game patwntas ta koumpia.
+    }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
